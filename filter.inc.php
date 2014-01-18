f820810d9162076124b9dea25c9218ee53c918d8
David Grant <acct-github@slicer.ca>
2014-01-17 21:32:44 -0800

    updates

---
+++ filter.inc.php
@@ -0,0 +1,37 @@
+<?php
+function filter_int(&$val)
+{
+	$val = (int)$val;
+}
+
+function filter_int_or_null(&$val)
+{
+	if(is_null($val)) return;
+	$val = (int)$val;
+}
+
+function filter_bool_or_null(&$val)
+{
+	if(is_null($val)) return;
+	$val = (int)$val;
+}
+
+function post_bool(&$val, $var)
+{
+	if(array_key_exists($var, $_POST)) {
+		$val = (int)$_POST[$var] ? 1 : 0;
+	}
+}
+
+function post_int(&$val, $var)
+{
+	if(array_key_exists($var, $_POST)) {
+		if($_POST[$var] == '') 
+			$val = NULL;
+		else 
+			$val = (int)$_POST[$var];
+	}
+}
+
+
+?>
