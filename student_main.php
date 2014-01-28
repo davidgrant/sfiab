38f73e96ac74a500dae91b3b24cf99dec03e6be1
David Grant <acct-github@slicer.ca>
2014-01-27 23:17:41 -0800

    update

--- student_main.php
+++ student_main.php
@@ -18,12 +18,9 @@ sfiab_page_begin("Student Main", 's_home');
 
 <div data-role="page" id="<?=$page_id?>"><div data-role="main" class="sfiab_page" > 
 
-	<h3>Student Home</h3>
+	<h3>Hello <?=$u['firstname']?>,</h3>
 
-	Under construction, only the first few menus on the left work, the rest throw an error.  
-
-	Don't try student registration yet, I know it doesn't work.
-
+	<p>Welcome to the new GVRSF registration system.  Help for most pages is available by clicking the information icon on the top right of the page.
 
 <?php
 	/* Check for any incoming project requests */
@@ -32,6 +29,34 @@ sfiab_page_begin("Student Main", 's_home');
 		<h3>Partner Request</h3>
 		<p>You have a partner request waiting, go to the <a href="student_partner.php">Project Partner</a> page to accept or reject it.
 <?php	} ?>
+
+
+<?php	if($u['s_status'] == 'incomplete') { ?>
+		<h3>Registration Status: <font color="red">Incomplete</font></h3>
+
+		<p>The red boxes in the left hand menu indicate which pages
+		still have information that needs to be filled out.
+<?php	} else if($u['s_status'] == 'complete') { ?>
+
+		<h3>Registration Status: <font color="orange">Almost Complete</font></h3>
+
+		<p>Thank you for completing your registration.  The final step of
+		registration is to print the signature page and send it in.
+
+		<p> We will be processing all registration forms during the two weeks
+		after registration closes 
+
+
+
+<?php	} else if($u['s_status'] == 'accepted') { ?>
+
+		<h3>Registration Status: <font color="green">Forms Received</font></h3>
+		
+		All your registration forms have been received and processed.
+<?php	} ?>
+
+	<h3>Forms and Documents</h3>
+	<p>We will be putting forms and documents here.  Most forms can already be found in the student handbook on <a href="http://www.gvrsf.ca">www.gvrsf.ca</a>
 	
 
 </div></div>
