5adfb7aca20d48db6a2f40902a15a131641638c8
David Grant <acct-github@slicer.ca>
2014-03-01 21:27:46 -0800

    add tcpdf6

--- tcpdf_6/config/tcpdf_config.php
+++ tcpdf_6/config/tcpdf_config.php
@@ -40,63 +40,56 @@
 // If you define the constant K_TCPDF_EXTERNAL_CONFIG, all the following settings will be ignored.
 // If you use the tcpdf_autoconfig.php, then you can overwrite some values here.
 
-if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
 
-	define('K_TCPDF_EXTERNAL_CONFIG',true);
-
-	$k_sfiab_path = $_SERVER['DOCUMENT_ROOT'].$config['document_root'].'/';
-	$k_tcpdf_path = $k_sfiab_path.'tcpdf_6/';
-	$k_sfiab_url = $config['document_root'].'/';
-	$k_tcpdf_url = $k_sfiab_url.'/tcpdf_6/';
 /**
  * Installation path (/var/www/tcpdf/).
  * By default it is automatically calculated but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_MAIN', $k_tcpdf_path);
+//define ('K_PATH_MAIN', '');
 
 /**
  * URL path to tcpdf installation folder (http://localhost/tcpdf/).
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_URL', $k_tcpdf_url);
+//define ('K_PATH_URL', '');
 
 /**
  * Path for PDF fonts.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_FONTS', K_PATH_MAIN.'fonts/');
+//define ('K_PATH_FONTS', K_PATH_MAIN.'fonts/');
 
 /**
  * Default images directory.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_IMAGES', $k_sfiab_path.'data/');
+//define ('K_PATH_IMAGES', '');
 
 /**
  * Deafult image logo used be the default Header() method.
  * Please set here your own logo or an empty string to disable it.
  */
-define ('PDF_HEADER_LOGO', 'logo.png');
+//define ('PDF_HEADER_LOGO', '');
 
 /**
  * Header logo image width in user units.
  */
-define ('PDF_HEADER_LOGO_WIDTH', 16);
+//define ('PDF_HEADER_LOGO_WIDTH', 0);
 
 /**
  * Cache directory for temporary files (full path).
  */
-define ('K_PATH_CACHE', '/tmp/');
+//define ('K_PATH_CACHE', '/tmp/');
 
 /**
  * Generic name for a blank image.
  */
-define ('K_BLANK_IMAGE', K_PATH_IMAGES.'_blank.png');
+define ('K_BLANK_IMAGE', '_blank.png');
 
 /**
  * Page format.
  */
-define ('PDF_PAGE_FORMAT', 'LETTER');
+define ('PDF_PAGE_FORMAT', 'A4');
 
 /**
  * Page orientation (P=portrait, L=landscape).
@@ -106,22 +99,22 @@ define ('PDF_PAGE_ORIENTATION', 'P');
 /**
  * Document creator.
  */
-define ('PDF_CREATOR', 'SFIAB');
+define ('PDF_CREATOR', 'TCPDF');
 
 /**
  * Document author.
  */
-define ('PDF_AUTHOR', 'SFIAB');
+define ('PDF_AUTHOR', 'TCPDF');
 
 /**
  * Header title.
  */
-define ('PDF_HEADER_TITLE',  $config['fair_name']);
+define ('PDF_HEADER_TITLE', 'TCPDF Example');
 
 /**
  * Header description string.
  */
-define ('PDF_HEADER_STRING', "");
+define ('PDF_HEADER_STRING', "by Nicola Asuni - Tecnick.com\nwww.tcpdf.org");
 
 /**
  * Document unit of measure [pt=point, mm=millimeter, cm=centimeter, in=inch].
@@ -141,22 +134,22 @@ define ('PDF_MARGIN_FOOTER', 10);
 /**
  * Top margin.
  */
-define ('PDF_MARGIN_TOP', 25);
+define ('PDF_MARGIN_TOP', 27);
 
 /**
  * Bottom margin.
  */
-define ('PDF_MARGIN_BOTTOM', 15);
+define ('PDF_MARGIN_BOTTOM', 25);
 
 /**
  * Left margin.
  */
-define ('PDF_MARGIN_LEFT', 10);
+define ('PDF_MARGIN_LEFT', 15);
 
 /**
  * Right margin.
  */
-define ('PDF_MARGIN_RIGHT', 10);
+define ('PDF_MARGIN_RIGHT', 15);
 
 /**
  * Default main font name.
@@ -186,7 +179,7 @@ define ('PDF_FONT_MONOSPACED', 'courier');
 /**
  * Ratio used to adjust the conversion of pixels to user units.
  */
-define ('PDF_IMAGE_SCALE_RATIO', 1);
+define ('PDF_IMAGE_SCALE_RATIO', 1.25);
 
 /**
  * Magnification factor for titles.
@@ -223,8 +216,6 @@ define('K_TCPDF_CALLS_IN_HTML', true);
  * If true and PHP version is greater than 5, then the Error() method throw new exception instead of terminating the execution.
  */
 define('K_TCPDF_THROW_EXCEPTION_ERROR', false);
-
-}
 
 //============================================================+
 // END OF FILE
--- tcpdf_6/examples/config/tcpdf_config_alt.php
+++ tcpdf_6/examples/config/tcpdf_config_alt.php
@@ -2,13 +2,13 @@
 //============================================================+
 // File name   : tcpdf_config.php
 // Begin       : 2004-06-11
-// Last Update : 2014-01-25
+// Last Update : 2013-05-16
 //
-// Description : Configuration file for TCPDF.
+// Description : Example of alternative configuration file for TCPDF.
 // Author      : Nicola Asuni - Tecnick.com LTD - www.tecnick.com - info@tecnick.com
 // License     : GNU-LGPL v3 (http://www.gnu.org/copyleft/lesser.html)
 // -------------------------------------------------------------------
-// Copyright (C) 2004-2014  Nicola Asuni - Tecnick.com LTD
+// Copyright (C) 2004-2013  Nicola Asuni - Tecnick.com LTD
 //
 // This file is part of TCPDF software library.
 //
@@ -29,74 +29,67 @@
 //============================================================+
 
 /**
- * Configuration file for TCPDF.
+ * Example of alternative configuration file for TCPDF.
  * @author Nicola Asuni
  * @package com.tecnick.tcpdf
  * @version 4.9.005
  * @since 2004-10-27
  */
 
-// IMPORTANT:
-// If you define the constant K_TCPDF_EXTERNAL_CONFIG, all the following settings will be ignored.
-// If you use the tcpdf_autoconfig.php, then you can overwrite some values here.
+/**
+ * Define the following constant to ignore the default configuration file.
+ */
+define ('K_TCPDF_EXTERNAL_CONFIG', true);
 
-if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
-
-	define('K_TCPDF_EXTERNAL_CONFIG',true);
-
-	$k_sfiab_path = $_SERVER['DOCUMENT_ROOT'].$config['document_root'].'/';
-	$k_tcpdf_path = $k_sfiab_path.'tcpdf_6/';
-	$k_sfiab_url = $config['document_root'].'/';
-	$k_tcpdf_url = $k_sfiab_url.'/tcpdf_6/';
 /**
  * Installation path (/var/www/tcpdf/).
  * By default it is automatically calculated but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_MAIN', $k_tcpdf_path);
+//define ('K_PATH_MAIN', '');
 
 /**
  * URL path to tcpdf installation folder (http://localhost/tcpdf/).
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_URL', $k_tcpdf_url);
+//define ('K_PATH_URL', '');
 
 /**
  * Path for PDF fonts.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_FONTS', K_PATH_MAIN.'fonts/');
+//define ('K_PATH_FONTS', K_PATH_MAIN.'fonts/');
 
 /**
  * Default images directory.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_IMAGES', $k_sfiab_path.'data/');
+define ('K_PATH_IMAGES', dirname(__FILE__).'/../images/');
 
 /**
  * Deafult image logo used be the default Header() method.
  * Please set here your own logo or an empty string to disable it.
  */
-define ('PDF_HEADER_LOGO', 'logo.png');
+define ('PDF_HEADER_LOGO', 'tcpdf_logo.jpg');
 
 /**
  * Header logo image width in user units.
  */
-define ('PDF_HEADER_LOGO_WIDTH', 16);
+define ('PDF_HEADER_LOGO_WIDTH', 30);
 
 /**
  * Cache directory for temporary files (full path).
  */
-define ('K_PATH_CACHE', '/tmp/');
+define ('K_PATH_CACHE', sys_get_temp_dir().'/');
 
 /**
  * Generic name for a blank image.
  */
-define ('K_BLANK_IMAGE', K_PATH_IMAGES.'_blank.png');
+define ('K_BLANK_IMAGE', '_blank.png');
 
 /**
  * Page format.
  */
-define ('PDF_PAGE_FORMAT', 'LETTER');
+define ('PDF_PAGE_FORMAT', 'A4');
 
 /**
  * Page orientation (P=portrait, L=landscape).
@@ -106,22 +99,22 @@ define ('PDF_PAGE_ORIENTATION', 'P');
 /**
  * Document creator.
  */
-define ('PDF_CREATOR', 'SFIAB');
+define ('PDF_CREATOR', 'TCPDF');
 
 /**
  * Document author.
  */
-define ('PDF_AUTHOR', 'SFIAB');
+define ('PDF_AUTHOR', 'TCPDF');
 
 /**
  * Header title.
  */
-define ('PDF_HEADER_TITLE',  $config['fair_name']);
+define ('PDF_HEADER_TITLE', 'TCPDF Example');
 
 /**
  * Header description string.
  */
-define ('PDF_HEADER_STRING', "");
+define ('PDF_HEADER_STRING', "by Nicola Asuni - Tecnick.com\nwww.tcpdf.org");
 
 /**
  * Document unit of measure [pt=point, mm=millimeter, cm=centimeter, in=inch].
@@ -141,22 +134,22 @@ define ('PDF_MARGIN_FOOTER', 10);
 /**
  * Top margin.
  */
-define ('PDF_MARGIN_TOP', 25);
+define ('PDF_MARGIN_TOP', 27);
 
 /**
  * Bottom margin.
  */
-define ('PDF_MARGIN_BOTTOM', 15);
+define ('PDF_MARGIN_BOTTOM', 25);
 
 /**
  * Left margin.
  */
-define ('PDF_MARGIN_LEFT', 10);
+define ('PDF_MARGIN_LEFT', 15);
 
 /**
  * Right margin.
  */
-define ('PDF_MARGIN_RIGHT', 10);
+define ('PDF_MARGIN_RIGHT', 15);
 
 /**
  * Default main font name.
@@ -186,7 +179,7 @@ define ('PDF_FONT_MONOSPACED', 'courier');
 /**
  * Ratio used to adjust the conversion of pixels to user units.
  */
-define ('PDF_IMAGE_SCALE_RATIO', 1);
+define ('PDF_IMAGE_SCALE_RATIO', 1.25);
 
 /**
  * Magnification factor for titles.
@@ -220,11 +213,9 @@ define('K_THAI_TOPCHARS', true);
 define('K_TCPDF_CALLS_IN_HTML', true);
 
 /**
- * If true and PHP version is greater than 5, then the Error() method throw new exception instead of terminating the execution.
+ * If true adn PHP version is greater than 5, then the Error() method throw new exception instead of terminating the execution.
  */
 define('K_TCPDF_THROW_EXCEPTION_ERROR', false);
-
-}
 
 //============================================================+
 // END OF FILE
