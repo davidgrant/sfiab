d4e52466bb4d4b40d5ba9e5169f04e69e0bf335b
David Grant <acct-github@slicer.ca>
2014-10-12 14:52:48 -0700

    new file

---
+++ debug.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+$debug_enable = true;
+
+function debug($str) 
+{
+	global $debug_enable;
+	global $config;
+
+	if($debug_enable !== true) return;
+
+	if(array_key_exists('fair_abbreviation', $config))
+		$fair = $config['fair_abbreviation'];
+	else
+		$fair = 'n/a';
+
+	$fp = fopen("/tmp/sfiab.log", "at");
+	fwrite($fp, $fair.":".$str);
+	fclose($fp);
+}
+
+?>
