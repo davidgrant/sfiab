1d5a64e6e78e37d5491bc0bcc3a5243cb126f07d
David Grant <acct-github@slicer.ca>
2014-12-14 22:58:40 -0800

    add db update

---
+++ updates/17.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `config` CHANGE `type` `type` ENUM( '', 'yesno', 'number', 'text', 'select', 'multisel', 'language', 'theme', 'timezone' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
+
+INSERT INTO `config` ( `var` , `val` , `category` , `type` , `type_values` , `order` , `description`) VALUES 
+	('project_number_format', 'CHN', 'Floorplanning', 'select', 'CHN=CCHHXX (2-digit Category, 2-digit Challenge, 2-digit Number)|X4=XXXX (4-digit sequential numbering)|c_X3_h=c XXX h (Category Shortform, 3-digit sequential, Challenge Shortform)', '', 'Project Numbering Format');
+
+INSERT INTO `cms` (`id` , `name` , `type` , `text` , `language` , `use`) VALUES 
+	(NULL , 's_main', 'pagetext', 'Extra information for the student main page', 'en', '1');
+
+
