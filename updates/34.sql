ac79e8d7f1b70ff86f031cd8c13c644f3140f66e
David Grant <acct-github@slicer.ca>
2015-11-29 15:34:21 -0800

    add mising

---
+++ updates/34.sql
@@ -0,0 +1,4 @@
+INSERT INTO `cms` (`id`, `name`, `type`, `head`,`desc`,`text`, `language`, `use`) VALUES
+	(NULL, 'committee', 'pagetext', 'Page: Committee Information', 'The text on the committee page linked off the main menu (this is not the committee member main page, this is the page about the committee that everyone case see)' , 
+	'<h3>Committee Information</h3><br/>Under Construction', 'en', 1);
+
