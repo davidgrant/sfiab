25165da5c025efcbea7d8949de5b21ad15f513b2
David Grant <acct-github@slicer.ca>
2016-01-30 00:03:32 -0800

    update

--- updates/36.sql
+++ updates/36.sql
@@ -1,3 +1,8 @@
 INSERT INTO `config` ( `var` , `val` , `category` , `type` , `type_values` , `order` , `name` , `description`) VALUES 
 	('smtp_type', 'webserver', 'Emails', 'select', 'webserver=Send mail directly from SFIAB|gmail=Send mail through a GMail Account|smtp=Send mail through a specified SMTP server', '100', '', 'SMTP Setting.  By default, choose "webserver".  If you get a lot of complaints about rejected mail, or mail not being delivered, you may need to send mail through the SMTP server for your hosted domain.'),
+	('smtp_host', '', 'Emails', 'text', '', '110', '', 'The SMTP host. Probably something like smtp.gmail.com'),
+	('smtp_port', '', 'Emails', 'number', '', '120', '', 'The SMTP port. Probably 25 or 587'),
+	('smtp_encryption', 'none', 'Emails', 'select', 'none=No Encryption|tls=TLS Encryption', '130', '', 'The SMTP encryption to use.  TLS is preferred'),
+	('smtp_username', '', 'Emails', 'text', '', '140', '', 'Username for authentication.  If blank, no authentication is attempted'),
+	('smtp_password', '', 'Emails', 'text', '', '150', '', 'Password for authentication.  If blank, no authentication is attempted') ;
 
