ee68215462abb7d53d7e1bf4c10cf6830c78c36f
David Grant <acct-github@slicer.ca>
2017-01-23 20:17:27 -0800

    add file

---
+++ updates/41.sql
@@ -0,0 +1,3 @@
+INSERT INTO `config` (`var`, `val`, `category`, `type`, `type_values`, `order`, `name`, `description`) VALUES 
+	('tours_temporarily_disabled', '0', 'Tours', 'yesno', '', '1', 'Disable Tours', 'Temporarily Disable Tours - Note, no student can complete their registration until this is enabled (assuming tours are enabled)');
+
