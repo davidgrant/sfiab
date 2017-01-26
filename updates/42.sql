c39b10a309f98cfe080ee69e869e9db7e12fc9f5
David Grant <acct-github@slicer.ca>
2017-01-25 19:53:10 -0800

    remove bodyhtml from emails

--- updates/42.sql
+++ updates/42.sql
@@ -1,4 +1,4 @@
 INSERT INTO `cms` (`id`, `name`, `type`, `head`,`desc`,`text`, `language`, `use`) VALUES
 	(NULL, 'student_payment_page', 'pagetext', 'Page: Student Payment', 'The text on the student payment page.  The payment page contains a breakdown and total of the fees due for the student, and the student registration ID' , 
 	'The payment information is shown below.  Please include your Registration ID on the cheque.', 'en', 1);
-
+ALTER TABLE `emails` DROP `bodyhtml`
