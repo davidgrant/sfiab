0fabe05c713c11dcbaeb9655f38a6e80d3bc721a
David Grant <acct-github@slicer.ca>
2017-01-30 23:09:03 -0800

    update to accepted

--- students_complete.php
+++ students_complete.php
@@ -13,12 +13,13 @@ $q = $mysqli->query("SELECT * FROM users WHERE
 					year='$year'
 					AND FIND_IN_SET('student',`roles`)>0
 					AND enabled = '1'
+					AND s_accepted = '1'
 					ORDER BY schools_id,lastname
 					 ");
 					 print($mysqli->error);
 ?>
 <pre>
-Students with a Completed Registration:
+Students Accepted (registration form received and accepted the fair):
 <?php while( ($s = $q->fetch_assoc()) ) { ?>
 <?=$s['firstname']?> <?=$s['lastname']?><br/>
 <?php } ?>
