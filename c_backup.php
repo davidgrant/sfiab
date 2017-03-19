7ee7112f8009029d6f077e73438b9748a7ed4b50
David Grant <dave@slicer.ca>
2017-03-19 12:01:42 -0700

    update exhibithall scripts and code

--- c_backup.php
+++ c_backup.php
@@ -129,14 +129,35 @@ case 'restore':
 	print("Received a file.\n");
 
 	/* Make sure the file uploaded successfully */
-	switch($_FILES['restore']['error']) {
-        case UPLOAD_ERR_OK:
-		break;
-        case UPLOAD_ERR_NO_FILE:
-        case UPLOAD_ERR_INI_SIZE:
-        case UPLOAD_ERR_FORM_SIZE:
-        default:
-		form_ajax_response(array('status'=>1, 'error'=>'File Upload Failed'.$_FILES['restore']['error']));
+	if($_FILES['restore']['error'] != UPLOAD_ERR_OK) {
+		$message = "";
+		switch($_FILES['restore']['error']) {
+		case UPLOAD_ERR_INI_SIZE: 
+			$message = "The uploaded file exceeds the upload_max_filesize directive in php.ini"; 
+			break; 
+		case UPLOAD_ERR_FORM_SIZE: 
+			$message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+			break; 
+		case UPLOAD_ERR_PARTIAL: 
+			$message = "The uploaded file was only partially uploaded"; 
+			break; 
+		case UPLOAD_ERR_NO_FILE: 
+			$message = "No file was uploaded"; 
+			break; 
+		case UPLOAD_ERR_NO_TMP_DIR: 
+			$message = "Missing a temporary folder"; 
+			break; 
+		case UPLOAD_ERR_CANT_WRITE: 
+			$message = "Failed to write file to disk"; 
+			break; 
+		case UPLOAD_ERR_EXTENSION: 
+			$message = "File upload stopped by extension"; 
+			break; 
+		default: 
+			$message = "Unknown upload error"; 
+			break; 
+		} 
+		form_ajax_response(array('status'=>1, 'error'=>'File Upload Failed: '.$message));
 		exit();
 	}
 	print("File OK.\n");
--- scripts/gvrsf_populate_exhibithall.php
+++ scripts/gvrsf_populate_exhibithall.php
@@ -22,11 +22,11 @@ $sx = 0.4;
 $sy = 22;
 $so = 90;
 
-/* Left: 1-16 */
+/* Left: 1-13 */
 $loc[] = array('eh'=>0, 'e'=>'1');
 $loc[] = array('y'=>-1.2, 'r'=>12);
 
-/* Top 17-35 */
+/* Top 14-35 */
 $loc[] = array('x'=>0, 'y'=>-6.0, 'o'=>90);
 $loc[] = array('x'=>1.2, 'r'=>21);
 
@@ -42,13 +42,13 @@ $loc[] = array('y'=>1.2, 'r'=>12);
 $loc[] = array('x'=>.8, 'o'=>-180);
 $loc[] = array('y'=>-1.2, 'r'=>12);
 
-/* Row 3 61-87 */
+/* Row 3 88-113 */
 $loc[] = array('x'=>3, 'o'=>180);
 $loc[] = array('y'=>1.2, 'r'=>12);
 $loc[] = array('x'=>.8, 'o'=>-180);
 $loc[] = array('y'=>-1.2, 'r'=>12);
 
-/* Row 4 61-87 */
+/* Row 4 114-139 */
 $loc[] = array('x'=>3, 'o'=>180);
 $loc[] = array('y'=>1.2, 'r'=>12);
 $loc[] = array('x'=>.8, 'o'=>-180);
@@ -60,31 +60,45 @@ $loc[] = array('y'=>1.2, 'r'=>12);
 $loc[] = array('x'=>.8, 'o'=>-180);
 $loc[] = array('y'=>-1.2, 'r'=>12);
 
-/* Row 6 166-193 */
+/* Row 6 166-191 */
 $loc[] = array('x'=>3, 'o'=>180, 'e'=>0);
 $loc[] = array('y'=>1.2, 'r'=>12);
 $loc[] = array('x'=>.8, 'o'=>-180);
 $loc[] = array('y'=>-1.2, 'r'=>12);
 
-/* Row 7 194-121 */
+/* Row 7 192-218 */
 $loc[] = array('x'=>3, 'o'=>180);
 $loc[] = array('y'=>1.2, 'r'=>12);
 $loc[] = array('x'=>.8, 'o'=>-180);
 $loc[] = array('y'=>-1.2, 'r'=>12);
 
-/* Top right 235-237 */
+/* Top right 218-220 */
 $loc[] = array('x'=>0,  'y'=>'-3.5', 'o'=>-90);
 $loc[] = array('x'=>1.2, 'r'=>2);
 
-/* Right wall */
+/* Right wall 221-233 */
 $loc[] = array('x'=>1, 'y'=>'1', 'o'=>270);
 $loc[] = array('y'=>1.2, 'r'=>12);
 
 
+/* Order of priority to remvoe floor locations to fit projects 
+ * Good for 233 down to 223 projects*/
+$priority = array();
+$priority[219] = 50;
+$priority[233] = 45;
+$priority[232] = 40;
+$priority[14]  = 35;
+$priority[13]  = 30;
+$priority[218] = 25;
+$priority[220] = 20;
+$priority[221] = 15;
+$priority[35]  = 10;
+$priority[231] = 5;
+
 
 $objects = array();
 
-$floornumber = 1;
+$floornumber = 0;
 $oid = 0;
 
 $x = $sx;
@@ -96,20 +110,28 @@ foreach($loc as $l) {
 	$r = array_key_exists('r', $l) ? $l['r'] : 1;
 	if(array_key_exists('eh', $l)) $eh = $l['eh'];
 	while($r > 0) {
+		$pri = 0;
 		if(array_key_exists('reset', $l) && $l['reset'] == true) {
 			$x = $l['x'];
 			$y = $l['y'];
 			$o = $l['o'];
 			if(array_key_exists('floornumber',$l)) 
 				$floornumber = $l['floornumber'];
+			if(array_key_exists('pri',$l)) 	$pri = $l['pri'];
 			$has_electricity = array_key_exists('e', $l) ? $l['e'] : 1;
 		} else {
 			if(array_key_exists('x', $l)) $x += $l['x'];
 			if(array_key_exists('y', $l)) $y += $l['y'];
 			if(array_key_exists('o', $l)) $o += $l['o'];
 			if(array_key_exists('e', $l)) $has_electricity = $l['e'];
+			if(array_key_exists('pri', $l)) $pri = $l['pri'];
 		}
 
+		$floornumber += 1;
+		if(array_key_exists($floornumber, $priority)) {
+			$pri = $priority[$floornumber];
+		} 
+			
 		$objects[$oid] = array( 'name' => "Location ".($oid+1),
 			'id' => $oid,
 			'w' => 1.2,
@@ -118,11 +140,12 @@ foreach($loc as $l) {
 			'y' => $y,
 			'o' => $o,
 			'type' => 'project',
-			'floor_number' => $floornumber++,
+			'floor_number' => $floornumber,
 			'eh_id' => $eh,
 			'challenges' => array(),
 			'cats' => array(),
 			'has_electricity' => $has_electricity,
+			'priority' => $pri,
 		);
 	//rint_r($objects[$oid]);
 		$oid++;
@@ -139,9 +162,9 @@ $mysqli->query("DELETE FROM exhibithall WHERE type='exhibithall'");
 1 	Ballroom 	exhibithall 	0 	0 	16.4 	35 	0 	0 	0 	1,2,3,4,5,6,7	1,2	1
 2 	Partyroom 	exhibithall 	0 	0 	8.2 	26.4 	0 	0 	0 	1,2,3,4,5,6,7	2,3	1
 */
-$mysqli->query("INSERT INTO exhibithall(`id`,`name`,`type`,`x`,`y`,`w`,`h`,`orientation`,`exhibithall_id`,`floornumber`,`challenges`,`cats`,`has_electricity`)
+$mysqli->query("INSERT INTO exhibithall(`id`,`name`,`type`,`x`,`y`,`w`,`h`,`orientation`,`exhibithall_id`,`floornumber`,`challenges`,`cats`,`has_electricity`,`priority`)
 		VALUES('', 'Nest','exhibithall',
-			'0','0','38.1','22.2','0','0','0','1,2,3,4,5,6,7','1,2,3','1')");
+			'0','0','38.1','22.2','0','0','0','1,2,3,4,5,6,7','1,2,3','1','0')");
 $base_eh_id = $mysqli->insert_id;
 
 /* Save to DB */
@@ -150,13 +173,14 @@ foreach($objects as $oid=>$o) {
 	$divs = serialize($o['challenges']);
 	$cats = serialize($o['cats']);
 	$eid = $base_eh_id+$o['eh_id'];
-	$mysqli->query("INSERT INTO exhibithall(`id`,`name`,`type`,`x`,`y`,`w`,`h`,`orientation`,`exhibithall_id`,`floornumber`,`challenges`,`cats`,`has_electricity`)
+	$mysqli->query("INSERT INTO exhibithall(`id`,`name`,`type`,`x`,`y`,`w`,`h`,`orientation`,`exhibithall_id`,`floornumber`,`challenges`,`cats`,`has_electricity`,`priority`)
 		VALUES('', '{$o['name']}','{$o['type']}',
 			'{$o['x']}','{$o['y']}','{$o['w']}','{$o['h']}',
 			'{$o['o']}',
 			'{$eid}',
 			'{$o['floor_number']}','$divs','$cats',
-			'{$o['has_electricity']}')");
+			'{$o['has_electricity']}',
+			'{$o['priority']}')");
 }
 echo "Done.\n";
 
