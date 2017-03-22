2b71011f37edd4d4f9bf54adcb9b7e168abb8094
David Grant <dave@slicer.ca>
2017-03-21 17:28:50 -0700

    add sql update

---
+++ updates/46.sql
@@ -0,0 +1,2 @@
+INSERT INTO `cms` (`id` , `name` , `type` ,`head`, `desc`,`text` , `language` , `use`) VALUES 
+	(NULL , 'j_main', 'pagetext', 'Page: Judge Main Page', 'Text on the judge login page', '', 'en', '1');
