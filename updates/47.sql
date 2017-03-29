178be57902969427f0f604bcb712251f9c7bec6b
David Grant <dave@slicer.ca>
2017-03-28 20:58:28 -0700

    update the last sql so that category shortforms cannot be null.  it crashes the scheduler

--- updates/47.sql
+++ updates/47.sql
@@ -1,2 +1,4 @@
 DELETE FROM `reports_items` WHERE `field`='req_table';
+ALTER TABLE  `categories` CHANGE  `shortform`  `shortform` CHAR( 8 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
+ALTER TABLE  `categories` CHANGE  `name`  `name` VARCHAR( 64 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
 
