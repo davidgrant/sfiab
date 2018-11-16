9edd0e72320599a0913183315b25bf6f0ca97274
David Grant <acct-gitlab@slicer.ca>
2018-11-15 20:38:00 -0800

    remove mcrypt_create_iv

--- fairs.inc.php
+++ fairs.inc.php
@@ -98,7 +98,7 @@ function fair_save($mysqli, &$f)
 function fair_create($mysqli)
 {
 	/* 128 char password in base64 so no mysql funniness is necessary */
-	$p = base64_encode(mcrypt_create_iv(96, MCRYPT_DEV_URANDOM));
+	$p = base64_encode(random_bytes(96));
 	$mysqli->query("INSERT INTO fairs (`password`) VALUES ('$p')");
 	$fair_id = $mysqli->insert_id;
 	return $fair_id;
